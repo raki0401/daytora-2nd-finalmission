9c5186a0250873ee68067f0c2791d5e36477b38c
牧大介 <raki0401@outlook.jp>
2021-02-03 07:16:14 +0900

    commentsの左まで完了

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//swiper
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 4,
+  spaceBetween: 40,
+  freeMode: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+});
