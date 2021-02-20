ded0995866a056f6a2c790117d65d5293dae0921
牧大介 <raki0401@outlook.jp>
2021-02-20 20:37:55 +0900

    すべてを入力したら送信できるの完成

--- js/contact.js
+++ js/contact.js
@@ -23,11 +23,13 @@ $(function () {
   let $submit = $('#js-submit');
   $('#js-form input, #js-form select, #js-form textarea').on('change', function () {
     if (
-      $('#js-form select[name="entry.829069922"]').val() !=="" &&
-      $('#js-form input[type="text"]').val() !== "" &&
+      $('#js-form select[name="entry.829069922"]').val() !=="選択してください" &&
+      $('#js-form input[name="entry.219301110"]').val() !== "" &&
+      $('#js-form input[name="entry.1763225746"]').val() !== "" &&
       $('#js-form input[type="email"]').val() !== "" &&
-      $('#js-form input[name="entry.1017764133"]').prop('checked') === true &&
-      $('#js-form textarea[name="entry.1186676634"]').val() !== ""
+      // $('#js-form input[name="entry.1017764133"]').prop('checked') === true &&
+      $('#js-form textarea[name="entry.1186676634"]').val() !== "" &&
+      $('#js-form input[name="entry.1028795190"]').prop('checked') === true
     ) {
       // 全て入力されたとき
       $submit.prop('disabled',false)
