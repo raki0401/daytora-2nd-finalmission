9ef1941131cb5be68ef1449524b495b6c68a8c68
牧大介 <raki0401@outlook.jp>
2021-02-21 23:23:21 +0900

    パスの修正。次はformのレスポンシブ

--- js/contact.js
+++ js/contact.js
@@ -21,9 +21,10 @@
 $(function () {
   // $("#js-submit").prop("disabled", true);
   let $submit = $('#js-submit');
+  
   $('#js-form input, #js-form select, #js-form textarea').on('change', function () {
     if (
-      $('#js-form select[name="entry.829069922"]').val() !=="選択してください" &&
+      $('#js-form select[name="entry.829069922"]').val() !== "選択してください" &&
       $('#js-form input[name="entry.219301110"]').val() !== "" &&
       $('#js-form input[name="entry.1763225746"]').val() !== "" &&
       $('#js-form input[type="email"]').val() !== "" &&
@@ -32,14 +33,40 @@ $(function () {
       $('#js-form input[name="entry.1028795190"]').prop('checked') === true
     ) {
       // 全て入力されたとき
-      $submit.prop('disabled',false)
-      $submit.addClass('-active') 
+      $submit.prop('disabled', false)
+      $submit.addClass('-active')
     } else {
       // 入力されていないとき
       $submit.removeClass('-active')
-      $submit.prop('disabled',true)
+      $submit.prop('disabled', true)
 
     }
   });
 });
-  
+
+
+$(function () { 
+let $form = $('#js-form'); 
+$form.submit(function (e) {
+  $.ajax({
+    url: $form.attr('action'),
+    data: $form.serialize(),
+    type: "POST",
+    dataType: "xml",
+    statusCode: {
+      0: function () {
+        //送信に成功したときの処理  
+        $form.slideUp()
+        $('#js-success').slideDown()
+      },
+      200: function () {
+        //送信に失敗したときの処理  
+        $form.slideUp()
+        $('#js-error').slideDown()
+      }
+    }
+  });
+  e.preventDefault();   // 送信後に画面遷移しないようにする処理
+  return false;
+});
+});
--- js/to-top.js
+++ js/to-top.js
@@ -29,13 +29,30 @@
 $(function(){ 
   $('a[href^="#"]').click(function(){ 
     let speed = 800; 
+    // let comment_place = -100;
     let id = $(this).attr("href"); 
     let target = $("#" == id ? "html" : id); 
     let position = $(target).offset().top; 
+    // let comment = $(target).offset().top + comment_place; 
+    
     $("html, body").animate({scrollTop: position},speed); 
+    // $("html, body").animate({scrollTop: comment},speed); 
     return false; 
   }); 
 }); 
+
+
+// 質問箇所です
+$(function(){
+  let comment = 120;
+  let position = $('#comments').offset().top - comment;
+  $('a[href="#comments"]').click(function(){
+    $("html, body").animate({scrollTop: position},speed);
+    return false;
+  });
+});
+
+
 
 // スクロールしたら表示・非表示
 $(function(){ 
