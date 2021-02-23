14c1b85d0d86c77c69b3cac5e2970a4636686f01
牧大介 <raki0401@outlook.jp>
2021-02-23 21:05:11 +0900

    完成。5日で完成のところ23日かかった

--- js/drawer.js
+++ js/drawer.js
@@ -1,4 +1,5 @@
 $(".hamburger-btn").click(function(){
+  $(".header").toggleClass("is-checker");
   $(".hamburger").toggleClass("is-checker");
   $(".hamburger-back").toggleClass("is-checker");
   $(".hamburger-btn").toggleClass("is-checker");
--- js/to-top.js
+++ js/to-top.js
@@ -26,43 +26,48 @@
 
 
 // #のときはTopに、それ以外は指定の場所にスクロール
-$(function(){ 
-  $('a[href^="#"]').click(function(){ 
-    let speed = 800; 
+$(function () {
+  $('a[href^="#"]').click(function () {
+    let speed = 800;
     // let comment_place = -100;
-    let id = $(this).attr("href"); 
-    let target = $("#" == id ? "html" : id); 
-    let position = $(target).offset().top; 
+    let header = $("#header").innerHeight();
+    let id = $(this).attr("href");
+    let target = $("#" == id ? "html" : id);
+    let position = $(target).offset().top - header;
     // let comment = $(target).offset().top + comment_place; 
-    
-    $("html, body").animate({scrollTop: position},speed); 
+    if ($(this).hasClass("a-litle")) {
+      position += 100;
+    }
+    if ($(this).hasClass("litle")) {
+      position -= 50;
+    }
+    $("html, body").animate({ scrollTop: position }, speed);
     // $("html, body").animate({scrollTop: comment},speed); 
-    return false; 
-  }); 
-}); 
-
-
-// 質問箇所です
-$(function(){
-  let comment = 120;
-  let position = $('#comments').offset().top - comment;
-  $('a[href="#comments"]').click(function(){
-    $("html, body").animate({scrollTop: position},speed);
     return false;
   });
 });
 
 
+// $(function(){
+//   let comment = 120;
+//   let position = $('#comments').offset().top - comment;
+//   $('a[href="#comments"]').click(function(){
+//     $("html, body").animate({scrollTop: position},speed);
+//     return false;
+//   });
+// });
+
+
 
 // スクロールしたら表示・非表示
-$(function(){ 
-  let pagetop = $('#page-top'); 
-  pagetop.hide(); 
-  $(window).on("scroll", function(){ 
-    if($(this).scrollTop() > 500){ 
-      $('#page-top').fadeIn(); 
-    }else{ 
-      $('#page-top').fadeOut(); 
-    } 
-  }); 
+$(function () {
+  let pagetop = $('#page-top');
+  pagetop.hide();
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 500) {
+      $('#page-top').fadeIn();
+    } else {
+      $('#page-top').fadeOut();
+    }
+  });
 }); 
